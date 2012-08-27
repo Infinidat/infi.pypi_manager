b8db4bf29b4a08ae178402e2edad013786067c64
Guy Rozendorn <guy@rzn.co.il>
2012-08-27 17:44:48 +0300

    TRIVIAL now also has Chishop for our local pypi

--- src/infi/vendata/pypi_manager/tests/__init__.py
+++ src/infi/vendata/pypi_manager/tests/__init__.py
@@ -1,6 +1,6 @@
 import unittest
 
-from .. import PyPI, PackageNotFound, download_package_from_global_pypi
+from .. import PyPI, PackageNotFound, download_package_from_global_pypi, Chishop
 
 class PyPI_TestCase(unittest.TestCase):
     def test_get_package__exists(self):
@@ -29,4 +29,33 @@ class PyPI_TestCase(unittest.TestCase):
         pypi = PyPI()
         self.assertEqual(pypi.get_latest_source_distribution_url('pycrypto'),
                          'http://pypi.python.org/packages/source/p/pycrypto/pycrypto-2.6.tar.gz')
+
+class Chishop_TestCase(unittest.TestCase):
+    def test_get_package__exists(self):
+        pypi = Chishop()
+        result = pypi.get_available_versions('infi.traceback')
+
+    def test_get_package__doesn_not_exist(self):
+        pypi = Chishop()
+        with self.assertRaises(PackageNotFound):
+            pypi.get_available_versions('abcxyz')
+
+    def test_get_source_url(self):
+        pypi = Chishop()
+        self.assertEqual(pypi.get_latest_source_distribution_url('ipython'),
+                         'http://pypi01.infinidat.com//media/dists/ipython-0.13.tar.gz#md5=42832d994731e9f64d0944ae864be4a1')
+
+    def test_download(self):
+        from .. import download_package_from_global_pypi
+        import stat
+        import os
+        path = download_package_from_global_pypi('infi.traceback')
+        size = os.stat(path)[stat.ST_SIZE]
+        self.assertGreater(size, 0)
+
+    def test_get_source_url__latest(self):
+        pypi = Chishop()
+        self.assertEqual(pypi.get_latest_source_distribution_url('pycrypto'),
+                         'http://pypi01.infinidat.com//media/dists/pycrypto-2.5.tar.gz#md5=2fdd4d7919840b5fbc800b8364cb8ca4')
+
 
