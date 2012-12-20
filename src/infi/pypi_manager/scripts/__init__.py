241048d99d8928051591e1258cdb3410c60b20aa
Guy Rozendorn <guy@rzn.co.il>
2012-12-20 14:32:27 +0200

    HOSTDEV-574 mirror_package now has a --recursive flag

--- src/infi/pypi_manager/scripts/__init__.py
+++ src/infi/pypi_manager/scripts/__init__.py
@@ -1,15 +1,47 @@
-from sys import argv, stdout
-from logging import basicConfig, DEBUG
-from .. import download_package_from_global_pypi
-from .. import upload_package_to_local_pypi
-from .. import extract_source_package_to_tempdir, chdir
+""" mirror_package
 
-def mirror_package(argv=argv[1:]):
-    argv.append(None)
-    basicConfig(stream=stdout, level=DEBUG)
-    package_name, distribution_type, release_version = argv[0], argv[1], argv[2]
+Usage:
+    mirror_package <package_name> <distribution_type> [<release_version>] [--recursive]
+
+"""
+
+import sys
+
+
+def mirror_package(argv=sys.argv[1:]):
+    from docopt import docopt
+    from ..__version__ import __version__
+    arguments = dict(docopt(__doc__, argv=argv, help=True, version=__version__))
+    _logging()
+    _mirror_package(arguments)
+
+
+def _logging():
+    from logging import basicConfig, DEBUG
+    basicConfig(stream=sys.stdout, level=DEBUG)
+
+
+def _mirror_package(arguments):
+    from .. import download_package_from_global_pypi
+    from .. import upload_package_to_local_pypi
+    from .. import extract_source_package_to_tempdir, chdir
+    package_name = arguments.get("<package_name>")
+    distribution_type = arguments.get("<distribution_type>")
+    release_version = arguments.get("<release_version>")
+    recursive = arguments.get("--recursive")
     package_source_archive = download_package_from_global_pypi(package_name, release_version)
     package_dir = extract_source_package_to_tempdir(package_source_archive)
     with chdir(package_dir):
+        pass
         upload_package_to_local_pypi(distribution_type)
+    if recursive:
+        _recursive(package_name, distribution_type, release_version)
+
+
+def _recursive(package_name, distribution_type, release_version):
+    from ..recursive import virtualenv
+    with virtualenv() as env:
+        env.easy_install(package_name, release_version)
+        for dependency in env.get_dependencies(package_name):
+            mirror_package([dependency, distribution_type, env.get_installed_version(dependency)])
 
