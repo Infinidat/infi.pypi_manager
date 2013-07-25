3b0f9ab9c94faf95af52114fd79579fc500f0f72
Arnon Yaari <arnony@infinidat.com>
2013-07-25 14:16:38 +0300

    get_licenses - be version-aware
    patch by amnong

--- src/infi/pypi_manager/scripts/licenses.py
+++ src/infi/pypi_manager/scripts/licenses.py
@@ -1,13 +1,26 @@
 from __future__ import print_function
 import sys
 import argparse
+import re
 
-from .. import PyPI
+from .. import PyPI, PackageNotFound
 from ..dependencies import get_dependencies
 
-def get_license(package_name):
+_dependency_string_pattern = re.compile('^(?P<name>.*?)((==|<=|>=|=<|=>)(?P<version>.*))?$')
+def _dependency_string_to_name_and_version(s):
+    match = _dependency_string_pattern.match(s)
+    if not match:
+        return s, None
+    return match.group('name'), match.group('version')
+
+def get_license(package_name, version=None, safe=False):
     pypi_client = PyPI()
-    info = pypi_client.get_release_data(package_name)
+    try:
+        info = pypi_client.get_release_data(package_name, version=version)
+    except PackageNotFound:
+        if safe:
+            return None
+        raise
     if not info:
         return 'N/A'
     if 'classifiers' in info:
@@ -20,18 +33,22 @@ def get_license(package_name):
     return 'N/A'
 
 def get_dependency_licenses(package_name, progress_callback=None):
-    licenses = {}
+    ''' Return licenses of package dependencies, as a list of tuples
+    (dependency, version, license) '''
+    licenses = []
     dependencies = list(get_dependencies(package_name))[1:]
     for i, (dependency, dependency_str) in enumerate(dependencies, 1):
+        version = _dependency_string_to_name_and_version(dependency_str)[1]
         if progress_callback:
-            progress_callback(dependency, i, len(dependencies), True)
-        licenses[dependency] = get_license(dependency)
+            progress_callback(dependency, version, i, len(dependencies), True)
+        license = get_license(dependency, version=version, safe=True) or '[PACKAGE NOT FOUND]'
+        licenses.append((dependency, version, license))
         if progress_callback:
-            progress_callback(dependency, i, len(dependencies), False)
+            progress_callback(dependency, version, i, len(dependencies), False)
     return licenses
 
-def _progress_callback(dep_name, dep_index, total_deps, is_before):
-    text = '[{}/{}] {}'.format(dep_index, total_deps, dep_name)
+def _progress_callback(dep_name, dep_version, dep_index, total_deps, is_before):
+    text = '[{}/{}] {} {}'.format(dep_index, total_deps, dep_name, dep_version or '')
     if is_before:
         print('\r' + text, end='', file=sys.stderr)
     else:
@@ -42,8 +59,8 @@ def run():
     parser.add_argument('name', help='Package name')
     args = parser.parse_args()
     licenses = get_dependency_licenses(args.name, progress_callback=_progress_callback)
-    for dependency, license in sorted(licenses.iteritems()):
-        print('{}: {}'.format(dependency, license))
+    for dependency, version, license in sorted(licenses, key=lambda keyval: keyval[0].lower()):
+        print('{}{}{}: {}'.format(dependency, ' ' if version else '', version or '', license))
 
 def main():
     try:
