b0675ecb801d01c75034b5ee4085860000ca34bf
Arnon Yaari <arnony@infinidat.com>
2015-07-16 16:52:48 +0300

    pypi01->pypi

--- src/infi/pypi_manager/tests/__init__.py
+++ src/infi/pypi_manager/tests/__init__.py
@@ -27,11 +27,11 @@ class PyPI_TestCase(unittest.TestCase):
 
 class DjangoPyPI_TestCase(unittest.TestCase):
     def test_get_package__exists(self):
-        pypi = DjangoPyPI("pypi01.infinidat.com")
+        pypi = DjangoPyPI("pypi.infinidat.com")
         result = pypi.get_available_versions('infi.traceback')
 
     def test_get_package__doesn_not_exist(self):
-        pypi = DjangoPyPI("pypi01.infinidat.com")
+        pypi = DjangoPyPI("pypi.infinidat.com")
         with self.assertRaises(PackageNotFound):
             pypi.get_available_versions('abcxyz')
 
@@ -43,6 +43,6 @@ class DjangoPyPI_TestCase(unittest.TestCase):
         self.assertGreater(size, 0)
 
     def test_get_source_url__latest(self):
-        pypi = DjangoPyPI("pypi01.infinidat.com")
+        pypi = DjangoPyPI("pypi.infinidat.com")
         self.assertEqual(pypi.get_latest_source_distribution_url('infi'),
-                         'http://pypi01.infinidat.com//media/dists/infi-0.0.1.tar.gz#md5=62f7db8e3017d37b5a99cc571d5b0f07')
+                         'http://pypi.infinidat.com//media/dists/infi-0.0.1.tar.gz#md5=62f7db8e3017d37b5a99cc571d5b0f07')
