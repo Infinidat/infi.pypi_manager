b098f074b7dd2e706d1f0bda98d83a141e84ae1b
Arnon Yaari <arnony@infinidat.com>
2015-08-27 11:09:37 +0300

    HOSTDEV-2210 compare_pypi_repos: stop using PyPI XMLRPC API

--- src/infi/pypi_manager/scripts/compare_pypi_repos.py
+++ src/infi/pypi_manager/scripts/compare_pypi_repos.py
@@ -1,4 +1,4 @@
-from .. import PyPI, DjangoPyPI
+from .. import PyPI, DjangoPyPI, PackageNotFound
 from prettytable import PrettyTable
 from pkg_resources import parse_version
 import requests
@@ -10,24 +10,16 @@ def get_versions_from_reference(reference_repo):
     search_result = re.findall("""href=["'](?:/pypi/)?([^/]+)/([^/]+)/["']""", reference_pypi_html)
     return dict((k, urllib.unquote(v)) for k, v in search_result)
 
-def get_version_dict_from_other(other_repo, repos_to_check):
-    search_result = list()
-    for i in range(0, len(repos_to_check), 10):
-        res = other_repo._client.search(dict(name=repos_to_check[i:i+10]), "or")
-        search_result.extend(res)
-    return dict([(repo["name"], repo["version"]) for repo in search_result if repo["name"] in repos_to_check])
-
 def compare_pypi_repos(reference_repo, other_repo):
     upgrade_table = PrettyTable(["Package", reference_repo.server, other_repo.server])
     downgrade_table = PrettyTable(["Package", reference_repo.server, other_repo.server])
     reference_repo_versions = get_versions_from_reference(reference_repo)
     repos_to_check = reference_repo_versions.keys()
-    other_repo_versions = get_version_dict_from_other(other_repo, repos_to_check)
     for name in sorted(repos_to_check):
         try:
-            reference_repo_version = reference_repo_versions[name]
-            other_repo_version = other_repo_versions[name]
-        except KeyError:
+            reference_repo_version = reference_repo.get_latest_version(name)
+            other_repo_version = other_repo.get_latest_version(name)
+        except PackageNotFound:
             continue
         if other_repo_version != reference_repo_version:
             if parse_version(reference_repo_version) < parse_version(other_repo_version):
@@ -35,10 +27,10 @@ def compare_pypi_repos(reference_repo, other_repo):
             else:
                 downgrade_table.add_row([name, reference_repo_version, other_repo_version])
 
-    print "Upgradable Packaages:"
+    print "Upgradable Packages:"
     print upgrade_table
     print
-    print "Downgradable Packaages:"
+    print "Downgradable Packages:"
     print downgrade_table
 
 
